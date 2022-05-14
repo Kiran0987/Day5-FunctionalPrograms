72cc99b713c237d6c03856351dba739fcfd0754e
kiran <sharmakiran237@gmail.com>
2022-05-14 14:25:03 +0530

    Added

--- Quadratic.java
+++ Quadratic.java
@@ -1,18 +1,11 @@
 package com.bridgelab;
 
-import java.util.Scanner;
-
 public class Quadratic {
 	public static void main(String[] Strings) {
 
-		Scanner input = new Scanner(System.in);
-
-		System.out.print("Input a: ");
-		double a = input.nextDouble();
-		System.out.print("Input b: ");
-		double b = input.nextDouble();
-		System.out.print("Input c: ");
-		double c = input.nextDouble();
+		double a = 1;
+		double b = 3;
+		double c = 2;
 
 		double result = b * b - 4.0 * a * c;
 
@@ -20,7 +13,8 @@ public class Quadratic {
 			double r1 = (-b + Math.pow(result, 0.5)) / (2.0 * a);
 			double r2 = (-b - Math.pow(result, 0.5)) / (2.0 * a);
 			System.out.println("The roots are " + r1 + " and " + r2);
-		} else if (result == 0.0) {
+		} 
+		else if (result == 0.0) {
 			double r1 = -b / (2.0 * a);
 			System.out.println("The root is " + r1);
 		} else {
